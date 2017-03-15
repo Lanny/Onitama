99b34b37c1c6553d626df7e9ed8e3b702537438f
RyanJenkins <Ryan.Jenkins@wellsfargo.com>
2017-03-15 12:55:37 -0700

    Watch static files when running server task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,38 @@ var gulp = require('gulp');
 var del = require('del');
 var nodemon = require('gulp-nodemon');
 
+var jsFiles = ['src/client/*.js', 'src/isomorphic/*.js'],
+  svgFiles = ['src/assets/svg/*.svg'],
+  htmlFiles = ['src/assets/svg/*.svg'];
+
 gulp.task('clean', function() {
   return del('build');
 });
 
 gulp.task('js', function() {
-  return gulp.src(['src/client/*.js', 'src/isomorphic/*.js'])
+  return gulp.src(jsFiles)
     .pipe(gulp.dest('build/static/js'));
 });
 
 gulp.task('svg', function() {
-  return gulp.src('src/assets/svg/*.svg')
+  return gulp.src(svgFiles)
     .pipe(gulp.dest('build/static/svg'));
 });
 
 gulp.task('html', function() {
-  return gulp.src('src/assets/html/*.html')
+  return gulp.src(htmlFiles)
     .pipe(gulp.dest('build/static'));
 });
 
-gulp.task('generate', ['js', 'svg', 'html'], function() {
-});
+gulp.task('generate', ['js', 'svg', 'html'], function() { });
 
 gulp.task('server', ['generate'], function() {
+  gulp.watch(jsFiles, ['js']);
+  gulp.watch(svgFiles, ['svg']);
+  gulp.watch(htmlFiles, ['html']);
+
   return nodemon({
-    script: 'src/server/index.js'
+    script: 'src/server/index.js',
+    watch: 'src/server/'
   });
 });
