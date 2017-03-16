18f076878c998192788e71e368944faee7d8add8
RyanJenkins <rjenkins@mail.sfsu.edu>
2017-03-16 01:43:37 -0700

    Jankily draw cards.

--- src/client/perspective.js
+++ src/client/perspective.js
@@ -1,5 +1,88 @@
 ;(function() {
   function wrap(d3, game) {
+    function drawGrid(g) {
+      g.selectAll('line.vert-line')
+        .data(d3.range(1,5))
+        .enter()
+        .append('line')
+        .classed('vert-line', true)
+        .attr('x1', function(d) { return d*20; })
+        .attr('x2', function(d) { return d*20; })
+        .attr('y1', function(d) { return 0; })
+        .attr('y2', function(d) { return 100; })
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1);
+
+      g.selectAll('line.horz-line')
+        .data(d3.range(1,5))
+        .enter()
+        .append('line')
+        .classed('horz-line', true)
+        .attr('y1', function(d) { return d*20; })
+        .attr('y2', function(d) { return d*20; })
+        .attr('x1', function(d) { return 0; })
+        .attr('x2', function(d) { return 100; })
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1);
+
+      g.append('rect')
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', 100)
+        .attr('height', 100)
+        .attr('fill', 'none')
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1);
+
+      return g;
+    }
+
+    function drawCard(g, card, flipped) {
+      var border = 12.5;
+
+      g.attr('transform', 'scale(0.16)')
+        .append('rect')
+        .attr('width', 250)
+        .attr('height', 125)
+        .attr('fill', 'none')
+        .attr('stroke', 'black')
+        .attr('stroke-width', 1);
+
+      var moveGrid = g.append('g')
+        .attr('transform', 'translate('+border+','+border+')');
+
+      drawGrid(moveGrid);
+
+      moveGrid.selectAll('rect.position')
+        .data(card.getMoves(null))
+        .enter()
+        .append('rect')
+        .classed('position', true)
+        .attr('x', function(d) { return (2-d[0])*20 })
+        .attr('y', function(d) { return (2-d[1])*20 })
+        .attr('width', 20)
+        .attr('height', 20)
+        .attr('fill', 'grey');
+
+      moveGrid.append('rect')
+        .attr('x', 40)
+        .attr('y', 40)
+        .attr('width', 20)
+        .attr('height', 20)
+        .attr('fill', 'black');
+
+
+      g.selectAll('text.card-caption')
+        .data([card])
+        .enter()
+        .append('text')
+        .classed('card-caption', true)
+        .text(function(d) { return d.name; })
+        .attr('text-anchor', 'middle')
+        .attr('x', 175)
+        .attr('y', 65);
+    }
+
     function Perspective(gameState, color, svg) {
       this.gameState = gameState;
       this.color = color;
@@ -8,8 +91,16 @@
       this._b = (this.color === 'WHITE') ? 0 : 4;
       this._m = (this.color === 'WHITE') ? 1 : -1;
 
-      this.renderGridLines();
-      this.renderPieces();
+      this.svg.attr('viewBox', '-1 -1 102 152')
+
+      this.svgBoard = this.svg.append('g')
+        .attr('transform', 'translate(0, 25)');
+
+      this.renderGridLines(this.svgBoard);
+      this.renderPieces(this.svgBoard);
+
+      var fff = this.svg.append('g');
+      drawCard(fff, this.gameState.whiteHand[0], false);
     }
 
     Perspective.prototype = {
@@ -19,51 +110,17 @@
       _gridYToSvgY: function(gridY) {
         return (gridY-this._b) * this._m * 20 + 10;
       },
-      renderGridLines: function() {
-        var self = this;
-
-        var gridLines = self.svg
-          .attr('viewBox', '-1 -1 102 102')
+      renderGridLines: function(board) {
+        var gridLines = board
           .append('g')
           .classed('grid-lines', true);
 
-        gridLines.selectAll('line.vert-line')
-          .data(d3.range(1,5))
-          .enter()
-          .append('line')
-          .classed('vert-line', true)
-          .attr('x1', function(d) { return d*20; })
-          .attr('x2', function(d) { return d*20; })
-          .attr('y1', function(d) { return 0; })
-          .attr('y2', function(d) { return 100; })
-          .attr('stroke', 'black')
-          .attr('stroke-width', 1);
-
-        gridLines.selectAll('line.horz-line')
-          .data(d3.range(1,5))
-          .enter()
-          .append('line')
-          .classed('horz-line', true)
-          .attr('y1', function(d) { return d*20; })
-          .attr('y2', function(d) { return d*20; })
-          .attr('x1', function(d) { return 0; })
-          .attr('x2', function(d) { return 100; })
-          .attr('stroke', 'black')
-          .attr('stroke-width', 1);
-
-        gridLines.append('rect')
-          .attr('x', 0)
-          .attr('y', 0)
-          .attr('width', 100)
-          .attr('height', 100)
-          .attr('fill', 'none')
-          .attr('stroke', 'black')
-          .attr('stroke-width', 1);
+        drawGrid(gridLines);
       },
-      renderPieces: function() {
+      renderPieces: function(board) {
         var self = this;
 
-        var piecesContainer = self.svg
+        var piecesContainer = board
           .append('g')
           .classed('pieces', true);
 
@@ -76,6 +133,9 @@
           .attr('x', function(d) { return self._gridXToSvgX(d.x) - 7.5; })
           .attr('y', function(d) { return self._gridYToSvgY(d.y) - 7.5; })
           .attr('href', function(d) { return d.piece.getSvgPath(); });
+
+      },
+      renderCard: function(g) {
 
       }
     };
