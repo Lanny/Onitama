bba22b896bfb6d7c40a7c282595e4152ca5ee03f
RyanJenkins <Ryan.Jenkins@wellsfargo.com>
2017-05-09 15:31:45 -0700

    Made card selection on new game screen toggleable.

--- src/client/new-game.js
+++ src/client/new-game.js
@@ -11,15 +11,27 @@
     'client-utils'
   ], function(d3, cards, {rectify, drawCard}) {
     const cardList = d3.select('#card-select-list');
+    var selection = cards.deck.map(c => c.group === 'Arcane Wonders');
 
-    rectify(cardList, 'li', cards.deck,
-      selection => selection.each((data, i, nodes) => {
-        const el = d3.select(nodes[i]);
-        const svg = el.append('svg')
-          .attr('viewBox', '0 0 250 125')
-          .style('width', '250px');
+    function rectifySelection(cardSelection, cardList) {
+      rectify(cardList, 'svg', selection,
+        selection => selection.each((data, i, nodes) => {
+          const svg = d3.select(nodes[i])
+            .attr('viewBox', '-5 -5 260 135')
+            .style('width', '250px');
 
-        drawCard(svg, data);
-      }));
+          drawCard(svg, cards.deck[i]);
+
+          svg.select('.background')
+            .attr('stroke', data ? 'green' : 'black')
+            .attr('stroke-width', data ? 5 : 1)
+            .on('click', () => {
+              cardSelection[i] = !cardSelection[i];
+              rectifySelection(cardSelection, cardList);
+            });
+        }));
+    }
+
+    rectifySelection(selection, cardList);
   });
 })();
